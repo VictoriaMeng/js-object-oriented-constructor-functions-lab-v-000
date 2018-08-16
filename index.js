a117e08791c8049abbe727a7e62fd334c1e0e5dd
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 17:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ function Driver(name, age, experience) {
   this.name = name;
   this.age = age;
   this.experience = experience;
+};
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
 }
